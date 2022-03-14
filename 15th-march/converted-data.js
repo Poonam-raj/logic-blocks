5418dffdf06136840eb39bb21b204ad6fa4250e4
Poonam <p_rajput@hotmail.co.uk>
2022-03-14 17:05:30 +0000

    restructuring convertDataPromise

--- 15th-march/converted-data.js
+++ 15th-march/converted-data.js
@@ -1,25 +1,31 @@
 // raw data exported as arr of objs
 
 const fs = require('fs/promises');
+async function convertDataPromise() {
+  let finalData;
+  await fs
+    .readFile('15th-march/data.txt', 'utf8')
+    .then((data) => {
+      const rawData = data;
+      const dataArr = rawData.split('\n');
+      let arrOfObjs = [];
+      dataArr.forEach((datum) => {
+        let infoSplit = datum.split('] ');
+        let cleanTimeStamp = infoSplit[0].split('[');
+        infoSplit[0] = cleanTimeStamp[1];
 
-let arrOfObjs = [];
-fs.readFile('./data.txt', 'utf8')
-  .then((response) => {
-    const rawData = response;
-    const dataArr = rawData.split('\n');
+        const innerObj = {};
+        innerObj.timestamp = infoSplit[0];
+        innerObj.info = infoSplit[1];
 
-    dataArr.forEach((datum) => {
-      let infoSplit = datum.split('] ');
-      let cleanTimeStamp = infoSplit[0].split('[');
-      infoSplit[0] = cleanTimeStamp[1];
-
-      const innerObj = {};
-      innerObj.timestamp = infoSplit[0];
-      innerObj.info = infoSplit[1];
-
-      arrOfObjs.push(innerObj);
+        arrOfObjs.push(innerObj);
+        finalData = arrOfObjs;
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  })
-  .catch((err) => console.log(err));
+  return finalData;
+}
 
-module.exports = arrOfObjs;
+module.exports = convertDataPromise;
