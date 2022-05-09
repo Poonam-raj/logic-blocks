567853e5706ebefc4a951d641584872c2ff65a2b
Poonam <p_rajput@hotmail.co.uk>
2022-05-09 10:11:37 +0100

    clearer explanations

--- 12th-may-sonarSweep/sonarSweep.js
+++ 12th-may-sonarSweep/sonarSweep.js
@@ -1,43 +1,40 @@
 const inputData = require('./data.js');
 
 function sonarSweep(depthData) {
-  // Creating an object of objects. Each object within is for each data entry of depth from the sonar
-  let windowTracker = {};
+  // Creating an object of objects representing each three-measurement window.
+  const windowTracker = {};
   for (let i = 0; i < depthData.length; i++) {
     windowTracker[i] = { count: 0, sum: 0 };
   }
 
   //   loop through depthData
   depthData.forEach((datum) => {
-    //   This count is needed because every depthData entry is present in three measurement windows EXCEPT the first two and last two data entries. This count makes sure we've utilised the data to the right amount.
-    let numCount = 0;
+    //   This count makes sure we've used each datum in three windows except the first two and last two data entries.
+    let useageCount = 0;
 
-    // for each depth datum loop through every key inside windowSums obj
+    // for each depth datum loop through all three-measurement windows
     for (let window in windowTracker) {
       // converting the window key to a Number data type
       window = +window;
 
-      if (numCount < 3 && windowTracker[window].count < 3) {
+      if (useageCount < 3 && windowTracker[window].count < 3) {
         /*  
-        If we have not passed three uses of the datum (the count starts at 0 for three uses we will have a count of 2) 
-        AND the current window has a count of less than three (aka less than three bits of data have been added to this window)
-        
+        If less than three bits of data have been added to this window AND current datum has been used less than three times.
         THEN
-        Always increment the window's count as we're looping through again.
         Always add the current depth datum to the sum of this window
-
+        Always increment the window's count.
         */
-        windowTracker[window].count++;
         windowTracker[window].sum += datum;
+        windowTracker[window].count++;
 
         if (window === 0 && windowTracker[0].count === 1) {
-          //   if the window's key is 0 (1st window) and it now has a count of 1, we can add three to the numCount so it skips this window in future as it has now capped the number of windows it needs. 1st window only has one data entry to it.
-          numCount += 3;
+          //   if this is the 1st window and it now has a count of 1, add 3 to the useageCount so it skips this window in the next loop as 1st window only has one data entry to it.
+          useageCount += 3;
         } else if (window === 1 && windowTracker[1].count === 1) {
-          // if the window's key is 1 (2nd window) and it now has a count of 1, we can add 2 to the numCount so it only loops once more for this window. 2nd window only has 2 entries to it.
-          numCount += 2;
+          // if this is the 2nd window and it now has a count of 1, we can add 2 to the useageCount so it only loops once more for this window. 2nd window only has 2 entries to it.
+          useageCount += 2;
         } else {
-          numCount++;
+          useageCount++;
         }
       }
     }
